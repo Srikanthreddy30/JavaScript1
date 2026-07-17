f9c42607bcc9f78962e8703ca6023ac31afc8e54
Srikanthreddy30 <srikanthporeddy2000@gmail.com>
2026-07-17 09:47:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+
+//Employee Registration
+const company = "Stackly";
+
+let employeeName = prompt("Enter employee name");
+let employeeId = "EMP101";
+
+console.log("Company :", company);
+console.log("Employee Name :", employeeName);
+console.log("Employee ID :", employeeId);
+
+
+
+
+//College Admission
+const college = "ABC Engineering College";
+
+let studentName = prompt("Enter student name");
+let course = prompt("Enter course");
+
+console.log("College :", college);
+console.log("Student :", studentName);
+console.log("Course :", course);
+
+
+
+
+//Food Order
+const resorent = "Food Park";
+let customerName = prompt("Enter the customer name");
+let foodName = prompt("Enter the food name");
+let quantity = 2;
+
+console.log ("Resorent:", resorent);
+console.log ("Customer:", customerName);
+console.log ("Food:", foodName);
+console.log ("Quantity:", quantity);
+
+//Hospital Registration
+const hospitalName = "Apollo";
+let patientName = prompt("Enter the patient name");
+let tokenNumber = 25;
+
+console.log("Hospital:",hospitalName);
+console.log("Patient Name:",patientName);
+console.log("Token:",tokenNumber);
+
+
+//Movie Ticket Booking
+const theatreName = "Asian";
+let movieNmae= prompt("Enter the movie name");
+let customerName= prompt ("Enter the customer name");
+let seatNumber= 6;
+
+console.log ("Theatre:",theatreName);
+console.log ("Movie:",movieNmae);
+console.log ("Customer Name:",customerName);
+console.log ("Seat Number:",seatNumber);
+
+
+//Mobile Shop
+const shop = "Smart Mobiles";
+
+let customerName = prompt("Enter customer name");
+let mobileModel = prompt("Enter mobile model");
+let price = 25000;
+
+console.log("Shop :", shop);
+console.log("Customer :", customerName);
+console.log("Mobile :", mobileModel);
+console.log("Price :", price);
+
+//Gym Membership
+const gym = "Fitness World";
+
+let memberName = prompt("Enter member name");
+let membershipFee = 5000;
+
+console.log("Gym :", gym);
+console.log("Member :", memberName);
+console.log("Membership Fee :", membershipFee);
+
+//ATM Welcome Screen
+const bank = "SBI";
+
+let userName = prompt("Enter your name");
+
+console.log("Welcome", userName);
+console.log("Bank :", bank);
+
+// //Bus Ticket
+const tavelsName= "Zing";
+let passengerName =prompt("Name of Passenger");
+let seatNumber= 46;
+
+console.log("Travels Name:",tavelsName);
+console.log("Passenger Name:",passengerName);
+console.log("Seat Number:",seatNumber);
+
+//Online Shopping
+const website = "Amazon";
+
+let productName = prompt("Enter product name");
+let customerName = prompt("Enter customer name");
+let price = 1999;
+
+console.log("Website :", website);
+console.log("Product :", productName);
+console.log("Customer :", customerName);
+console.log("Price :", price);
+
+// //console.log()
+
+console.log("Registration Successful");
+
+//console.error()
+console.error("Payment Failed");
+
+//console.warn()
+console.warn("Battery Low");
+
+//console.clear()
+console.log("Loading...");
+console.log("Welcome");
+
+console.clear();
+
+//alert()
+alert("Welcome to Amazon");
+
+
+//confirm()
+let result = confirm("Do you want to logout?");
+
+console.log(result);
+
+//prompt()
+let city =prompt("Enter the city")
+console.log("Your City:", city);
+
+//Variable Practice
+var product="Laptop";
+product="Mobile";
+var product="Tablet";
+
+console.log(product);
+
+//let
+let salay= "2500";
+salary="30000";
+ console.log(salary);
+
+ //Const
+ const company = "Google";
+
+console.log(company);
+
